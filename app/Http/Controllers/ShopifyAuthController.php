55296deccd78bbf8bf76ebd589da57611db74d7a
Hanifa Anum <hanifaanum5@gmail.com>
2026-02-23 13:28:19 -0800

    Updated Shop Model

--- app/Http/Controllers/ShopifyAuthController.php
+++ app/Http/Controllers/ShopifyAuthController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Http;
-
 use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
 
 class ShopifyAuthController extends Controller
 
