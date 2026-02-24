29f75b33cd62433d113c63f80e4724013cc2222e
Hanifa Anum <hanifaanum5@gmail.com>
2026-02-24 12:31:28 -0800

    Order API Update

--- routes/api.php
+++ routes/api.php
@@ -25,9 +25,10 @@ Route::get('booking/{trackNumber}', [BookingController::class, 'trackBooking']);
 
 Route::post('/connect-app', [LoginController::class, 'connect']);
 
+Route::get('/orders', [\App\Http\Controllers\Shopify\OrderController::class, 'getOrders']);
 Route::middleware(['shopify.session'])->group(function () {
 
-    Route::get('/orders', [\App\Http\Controllers\Shopify\OrderController::class, 'getOrders']);
+    
 
     Route::post('/push-orders', [\App\Http\Controllers\Shopify\OrderController::class, 'pushOrders']);
 
